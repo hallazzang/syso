97da55353a868495dc6b3dce5861b84930281582
Hanjun Kim <hallazzang@gmail.com>
2019-06-02 14:27:40 +0900

    implement Section.WriteTo

--- rsrc/section.go
+++ rsrc/section.go
@@ -2,7 +2,6 @@ package rsrc
 
 import (
 	"encoding/binary"
-	"fmt"
 	"io"
 	"unicode/utf16"
 
@@ -11,13 +10,13 @@ import (
 
 // Section is a .rsrc section.
 type Section struct {
-	rootDir *directory
+	rootDir *resourceDirectory
 }
 
 // New returns newly created .rsrc section.
 func New() *Section {
 	return &Section{
-		rootDir: &directory{},
+		rootDir: &resourceDirectory{},
 	}
 }
 
@@ -26,29 +25,54 @@ func (s *Section) Name() string {
 	return ".rsrc"
 }
 
-func (s *Section) freeze() error {
+func (s *Section) freeze() {
 	var offset uint32
-	_ = offset
 
-	// TODO: work on this function. added offset fields to each structures
-
-	if err := s.rootDir.walk(func(dir *directory) error {
-		offset += uint32(binary.Size(&resourceDirectory{}))
+	s.rootDir.walk(func(dir *resourceDirectory) error {
+		// TODO: instead of calculating size of newly created dummy structure,
+		// use fixed constant.
+		dir.offset = offset
+		offset += uint32(binary.Size(&rawResourceDirectory{}))
+		for _, e := range dir.entries() {
+			e.offset = offset
+			offset += uint32(binary.Size(&rawResourceDirectoryEntry{}))
+		}
 		return nil
-	}); err != nil {
-		return err
-	}
+	})
 
-	return nil
+	s.rootDir.walk(func(dir *resourceDirectory) error {
+		for _, str := range dir.strings {
+			str.offset = offset
+			// TODO: should we encode string to calculate its utf-16
+			// encoded size? better solution may exist.
+			offset += 2 + uint32(len(utf16.Encode([]rune(str.string))))
+		}
+		return nil
+	})
+
+	s.rootDir.walk(func(dir *resourceDirectory) error {
+		for _, e := range dir.dataEntries() {
+			e.offset = offset
+			offset += uint32(binary.Size(&resourceDataEntry{}))
+		}
+		return nil
+	})
+
+	s.rootDir.walk(func(dir *resourceDirectory) error {
+		for _, d := range dir.datas() {
+			d.offset = offset
+			offset += uint32(d.blob.Size())
+		}
+		return nil
+	})
 }
 
 // WriteTo writes section data to w.
 func (s *Section) WriteTo(w io.Writer) (int64, error) {
 	var written int64
 
-	if err := s.rootDir.walk(func(dir *directory) error {
-		fmt.Printf("%08X %d\n", written, dir.characteristics)
-		n, err := common.BinaryWriteTo(w, &resourceDirectory{
+	if err := s.rootDir.walk(func(dir *resourceDirectory) error {
+		n, err := common.BinaryWriteTo(w, &rawResourceDirectory{
 			Characteristics:     dir.characteristics,
 			NumberOfNameEntries: uint16(len(dir.nameEntries)),
 			NumberOfIDEntries:   uint16(len(dir.idEntries)),
@@ -58,13 +82,27 @@ func (s *Section) WriteTo(w io.Writer) (int64, error) {
 		}
 		written += n
 
-		for _, e := range dir.nameEntries {
-			// TODO: implement
-			_ = e
-		}
-		for _, e := range dir.idEntries {
-			// TODO: implement
-			_ = e
+		for _, e := range dir.entries() {
+			var i uint32
+			if e.name != nil {
+				i = e.name.offset
+			} else {
+				i = uint32(*e.id)
+			}
+			var o uint32
+			if e.dataEntry != nil {
+				o = e.dataEntry.offset
+			} else {
+				o = e.subdirectory.offset
+			}
+			n, err := common.BinaryWriteTo(w, &rawResourceDirectoryEntry{
+				NameOffsetOrIntegerID:               i,
+				DataEntryOffsetOrSubdirectoryOffset: o,
+			})
+			if err != nil {
+				return err
+			}
+			written += n
 		}
 
 		return nil
@@ -72,15 +110,15 @@ func (s *Section) WriteTo(w io.Writer) (int64, error) {
 		return written, err
 	}
 
-	if err := s.rootDir.walk(func(dir *directory) error {
-		for _, e := range dir.nameEntries {
-			s := utf16.Encode([]rune(*e.name))
-			n, err := common.BinaryWriteTo(w, uint16(len(s)))
+	if err := s.rootDir.walk(func(dir *resourceDirectory) error {
+		for _, str := range dir.strings {
+			u := utf16.Encode([]rune(str.string))
+			n, err := common.BinaryWriteTo(w, uint16(len(u)))
 			if err != nil {
 				return err
 			}
 			written += n
-			n, err = common.BinaryWriteTo(w, s)
+			n, err = common.BinaryWriteTo(w, u)
 			if err != nil {
 				return err
 			}
@@ -91,7 +129,30 @@ func (s *Section) WriteTo(w io.Writer) (int64, error) {
 		return written, err
 	}
 
-	if err := s.rootDir.walk(func(dir *directory) error {
+	if err := s.rootDir.walk(func(dir *resourceDirectory) error {
+		for _, e := range dir.dataEntries() {
+			n, err := common.BinaryWriteTo(w, &rawResourceDataEntry{
+				DataRVA: e.data.offset,
+				Size:    uint32(e.data.blob.Size()),
+			})
+			if err != nil {
+				return err
+			}
+			written += n
+		}
+		return nil
+	}); err != nil {
+		return written, err
+	}
+
+	if err := s.rootDir.walk(func(dir *resourceDirectory) error {
+		for _, d := range dir.datas() {
+			n, err := io.CopyN(w, d.blob, d.blob.Size())
+			if err != nil {
+				return err
+			}
+			written += n
+		}
 		return nil
 	}); err != nil {
 		return written, err
@@ -102,13 +163,13 @@ func (s *Section) WriteTo(w io.Writer) (int64, error) {
 
 // AddIconByID adds an icon resource identified by an integer id
 // to section.
-func (s *Section) AddIconByID(id int, data common.Blob) error {
+func (s *Section) AddIconByID(id int, data Blob) error {
 	panic("not implemented")
 	return nil
 }
 
 // AddIconByName adds an icon resource identified by name to section.
-func (s *Section) AddIconByName(name string, data common.Blob) error {
+func (s *Section) AddIconByName(name string, data Blob) error {
 	panic("not implemented")
 	return nil
 }
