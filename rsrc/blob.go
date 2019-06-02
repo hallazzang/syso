663855985149f3774f2c4bfb595f3f825ed247e5
Hanjun Kim <hallazzang@gmail.com>
2019-06-02 14:53:50 +0900

    fix typo

--- rsrc/blob.go
+++ rsrc/blob.go
@@ -2,7 +2,7 @@ package rsrc
 
 import "io"
 
-// Blob represents arbitary data that can be embedded in an object file.
+// Blob represents arbitrary data that can be embedded in an object file.
 type Blob interface {
 	io.Reader
 	Size() int64
