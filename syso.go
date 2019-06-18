2e0da5b775e837520e266845d50f37ce2403c48b
Hanjun Kim <hallazzang@gmail.com>
2019-06-18 23:56:49 +0900

    add EmbedManifest function, refactor code

--- syso.go
+++ syso.go
@@ -6,33 +6,37 @@ import (
 	"os"
 
 	"github.com/hallazzang/syso/pkg/coff"
+	"github.com/hallazzang/syso/pkg/common"
 	"github.com/hallazzang/syso/pkg/ico"
 	"github.com/hallazzang/syso/pkg/rsrc"
 	"github.com/pkg/errors"
 )
 
-type Icon struct {
-	Name string
+// FileResource represents a file resource that can be found at Path.
+type FileResource struct {
 	ID   int
+	Name string
 	Path string
 }
 
-func (i *Icon) Validate() error {
-	if i.Path == "" {
+// Validate returns an error if the resource is invalid.
+func (r *FileResource) Validate() error {
+	if r.Path == "" {
 		return errors.New("no file path given")
-	} else if i.ID == 0 && i.Name == "" {
+	} else if r.ID == 0 && r.Name == "" {
 		return errors.New("neither id nor name given")
-	} else if i.ID != 0 && i.Name != "" {
+	} else if r.ID != 0 && r.Name != "" {
 		return errors.New("id and name cannot be set together")
-	} else if i.ID < 0 {
-		return errors.Errorf("invalid id: %d", i.ID)
+	} else if r.ID < 0 {
+		return errors.Errorf("invalid id: %d", r.ID)
 	}
 	return nil
 }
 
 // Config is a syso config data.
 type Config struct {
-	Icons []*Icon
+	Icons    []*FileResource
+	Manifest *FileResource
 }
 
 // ParseConfig reads JSON-formatted syso config from r and returns Config object.
@@ -42,12 +46,8 @@ func ParseConfig(r io.Reader) (*Config, error) {
 		return nil, errors.Wrap(err, "failed to decode JSON")
 	}
 	for i, icon := range c.Icons {
-		if icon.Name == "" && icon.ID == 0 {
-			return nil, errors.Errorf("icon #%d's name and id are both empty", i)
-		} else if icon.Path == "" {
-			return nil, errors.Errorf("icon #%d's path is empty", i)
-		} else if icon.ID < 0 {
-			return nil, errors.Errorf("icon #%d: bad id; %d", i, icon.ID)
+		if err := icon.Validate(); err != nil {
+			return nil, errors.Wrapf(err, "failed to validate icon #%d", i)
 		}
 		for j, icon2 := range c.Icons[:i] {
 			if icon.ID != 0 && icon2.ID != 0 && icon2.ID == icon.ID {
@@ -57,28 +57,22 @@ func ParseConfig(r io.Reader) (*Config, error) {
 			}
 		}
 	}
+	if c.Manifest != nil {
+		if err := c.Manifest.Validate(); err != nil {
+			return nil, errors.Wrap(err, "failed to validate manifest")
+		}
+	}
 	return &c, nil
 }
 
 // EmbedIcon embeds icon into c.
-func EmbedIcon(c *coff.File, icon *Icon) error {
+func EmbedIcon(c *coff.File, icon *FileResource) error {
 	if err := icon.Validate(); err != nil {
 		return errors.Wrap(err, "invalid icon")
 	}
-	s, err := c.Section(".rsrc")
+	r, err := getOrCreateRSRCSection(c)
 	if err != nil {
-		if err == coff.ErrSectionNotFound {
-			s = rsrc.New()
-			if err := c.AddSection(s); err != nil {
-				return errors.New("failed to add new .rsrc section")
-			}
-		} else {
-			return errors.Wrap(err, "failed to get .rsrc section")
-		}
-	}
-	r, ok := s.(*rsrc.Section)
-	if !ok {
-		return errors.New(".rsrc section is not a valid rsrc section")
+		return errors.Wrap(err, "failed to get or create .rsrc section")
 	}
 	f, err := os.Open(icon.Path)
 	if err != nil {
@@ -101,9 +95,56 @@ func EmbedIcon(c *coff.File, icon *Icon) error {
 		err = r.AddIconGroupByName(icon.Name, icons)
 	}
 	if err != nil {
-		return errors.Wrap(err, "failed to add icon group")
+		return errors.Wrap(err, "failed to add icon group resource")
 	}
 	return nil
+}
+
+func EmbedManifest(c *coff.File, manifest *FileResource) error {
+	if err := manifest.Validate(); err != nil {
+		return errors.Wrap(err, "invalid manifest")
+	}
+	r, err := getOrCreateRSRCSection(c)
+	if err != nil {
+		return errors.Wrap(err, "failed to get or create .rsrc section")
+	}
+	f, err := os.Open(manifest.Path)
+	if err != nil {
+		return errors.Wrap(err, "failed to open manifest file")
+	}
+	defer f.Close()
+	b, err := common.NewBlob(f)
+	if err != nil {
+		return err
+	}
+	if manifest.ID != 0 {
+		err = r.AddManifestByID(manifest.ID, b)
+	} else {
+		err = r.AddManifestByName(manifest.Name, b)
+	}
+	if err != nil {
+		return errors.Wrap(err, "failed to add manifest resource")
+	}
+	return nil
+}
+
+func getOrCreateRSRCSection(c *coff.File) (*rsrc.Section, error) {
+	s, err := c.Section(".rsrc")
+	if err != nil {
+		if err == coff.ErrSectionNotFound {
+			s = rsrc.New()
+			if err := c.AddSection(s); err != nil {
+				return nil, errors.New("failed to add new .rsrc section")
+			}
+		} else {
+			return nil, errors.Wrap(err, "failed to get .rsrc section")
+		}
+	}
+	r, ok := s.(*rsrc.Section)
+	if !ok {
+		return nil, errors.New("the .rsrc section is not a valid rsrc section")
+	}
+	return r, nil
 }
 
 func findPossibleID(r *rsrc.Section, from int) int {
