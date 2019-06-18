b0898adfaaad4380c15d3eafca095f28a854de32
Hanjun Kim <hallazzang@gmail.com>
2019-06-18 23:01:39 +0900

    introduce FileBlob

--- pkg/common/blob.go
+++ pkg/common/blob.go
@@ -1,9 +1,33 @@
 package common
 
-import "io"
+import (
+	"io"
+	"os"
+)
 
 // Blob represents arbitrary data that can be embedded in an object file.
 type Blob interface {
 	io.Reader
 	Size() int64
+}
+
+// FileBlob wraps os.File and implements Blob interface.
+type FileBlob struct {
+	*os.File
+}
+
+// NewFileBlob returns new FileBlob that wraps file f.
+func NewFileBlob(f *os.File) *FileBlob {
+	return &FileBlob{
+		File: f,
+	}
+}
+
+// Size returns file's size.
+func (b *FileBlob) Size() int64 {
+	fs, err := b.File.Stat()
+	if err != nil {
+		panic(err)
+	}
+	return fs.Size()
 }
